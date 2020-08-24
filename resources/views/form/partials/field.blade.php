895afcaf81b5516f060a03a5dc4fa3b5e5529066
Ryan Thompson <service@aiwebsystems.com>
2020-08-24 16:42:37 -0500

    Working idea

--- resources/views/form/partials/field.blade.php
+++ resources/views/form/partials/field.blade.php
@@ -4,7 +4,7 @@
 
     <div class="field__input">
         <?php // @todo Replace this with $field->input(); // __toString() == $input->render() ?>
-        @include('ui::input/' . ($field->input ?: 'input'), ['input' => $field])
+        @include('ui::input/' . (Arr::get($field->input, 'type', 'input')), ['input' => $field])
     </div>
 
 </div>
