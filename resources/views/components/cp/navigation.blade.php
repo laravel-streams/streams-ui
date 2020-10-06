f56f18c8dd8cbbbc5134ee2c84fed813063a1f10
Ryan Thompson <ryan@pyrocms.com>
2020-10-06 03:19:20 -0500

    Ugh

--- resources/views/components/cp/navigation.blade.php
+++ resources/views/components/cp/navigation.blade.php
@@ -1,9 +1,9 @@
 @section('navigation')
 <nav class="flex-1 px-2 space-y-1">
-    @foreach ($cp->navigation->filter(function($item) {
-        return !$item->parent;
+    @foreach ($cp->navigation->filter(function($link) {
+        return !$link->parent;
     }) as $key => $item)
-    <x-streams::cp.navigation-link href="/ui/{{ $item->id }}/table" :link="$item"/>
+    <x-streams::cp.navigation-link href="/ui/{{ $item->id }}/table" :link="$item" />
     @endforeach
 </nav>
 @show
