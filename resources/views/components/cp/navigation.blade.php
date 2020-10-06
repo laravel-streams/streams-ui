718346b22c6ce29f7c80ff14e77054bf261ae360
Ryan Thompson <ryan@pyrocms.com>
2020-10-06 02:46:49 -0500

    Hm

--- resources/views/components/cp/navigation.blade.php
+++ resources/views/components/cp/navigation.blade.php
@@ -1,7 +1,7 @@
 @section('navigation')
 <nav class="flex-1 px-2 space-y-1">
     @foreach ($cp->navigation as $key => $item)
-    <x-streams::cp.navigation-link href="/ui/{{ $item->id }}/table" :link="$item"></x-streams::cp.navigation-link>
+    <x-streams::cp.navigation-link href="/ui/{{ $item->id }}/table" :link="$item"/>
     @endforeach
 </nav>
 @show
