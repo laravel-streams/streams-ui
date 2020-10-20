3d774541df5dc29128165fb569da3aa94aa8c921
Ryan Thompson <service@aiwebsystems.com>
2020-10-20 03:37:25 -0500

    CS

--- resources/views/forms/layout.blade.php
+++ resources/views/forms/layout.blade.php
@@ -1,22 +1,17 @@
 <div class="form__layout">
-    @if ($form->fields->isEmpty())
-        {{ trans("ui::message.no_fields_available") }}
+    @if ($form->sections->isnotEmpty())
+        @foreach ($form->sections as $section)
+            @if (isset($section['view']))
+                @include($section['view'])
+            @elseif (isset($section['html']))
+                {!! View::parse($section['html']) !!}
+            @elseif (isset($section['tabs']))
+                @include('ui::forms.tabs')
+            @else
+                @include('ui::forms.section')
+            @endif
+        @endforeach
     @else
-        @if ($form->sections->isnotEmpty())
-            @foreach ($form->sections as $section)
-                @if (isset($section['view']))
-                    @include($section['view'])
-                @elseif (isset($section['html']))
-                    {!! View::parse($section['html']) !!}
-                @elseif (isset($section['tabs']))
-                    @include('ui::forms.tabs')
-                @else
-                    @include('ui::forms.section')
-                @endif
-            @endforeach
-        @else
-            @include('ui::forms.default')
-        @endif
-        
+        @include('ui::forms.default')
     @endif
 </div>
