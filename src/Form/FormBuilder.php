0a6e2eb21d4784025996201a833d82bf7ad427d2
Ryan Thompson <ryan@pyrocms.com>
2020-11-06 00:21:21 -0600

    Build fields

--- src/Form/FormBuilder.php
+++ src/Form/FormBuilder.php
@@ -3,12 +3,15 @@
 namespace Streams\Ui\Form;
 
 use Streams\Ui\Form\Form;
+use Illuminate\Support\Arr;
 use Streams\Core\Stream\Stream;
 use Streams\Ui\Support\Builder;
 use Illuminate\Support\Facades\App;
 use Streams\Ui\Form\Command\SaveForm;
 use Streams\Ui\Form\Workflows\BuildForm;
 use Streams\Ui\Form\Workflows\QueryForm;
+use Streams\Core\Support\Facades\Resolver;
+use Streams\Core\Support\Facades\Evaluator;
 use Streams\Ui\Form\Workflows\ValidateForm;
 use Streams\Core\Repository\Contract\RepositoryInterface;
 use Streams\Ui\Form\Component\Field\Workflows\BuildFields;
@@ -58,8 +61,30 @@ class FormBuilder extends Builder
             'component' => 'form',
             'form' => Form::class,
 
+            'steps' => [
+                'make_component' => [$this, 'make'],
+
+                'query_entry' => [$this, 'queryEntry'],
+
+                'authorize_form' => [$this, 'authorizeForm'],
+
+                'set_validation' => [$this, 'setValidation'],
+
+                'build_fields' => [$this, 'buildFields'],
+                'build_actions' => [$this, 'buildActions'],
+                'build_buttons' => [$this, 'buildButtons'],
+                'build_sections' => [$this, 'buildSections'],
+
+                'load_values' => [$this, 'loadValues'],
+
+                'validate_form' => [$this, 'validateForm'],
+                'flash_messages' => [$this, 'flashMessages'],
+
+                'handle_request' => [$this, 'handleRequest'],
+            ],
+
             'workflows' => [
-                'build' => BuildForm::class,
+                //'build' => BuildForm::class,
                 'query' => QueryForm::class,
                 'fields' => BuildFields::class,
                 'actions' => BuildActions::class,
@@ -133,6 +158,80 @@ class FormBuilder extends Builder
         $this->fire('validated', ['builder' => $this]);
 
         return $this;
+    }
+
+    public function queryEntry()
+    {
+        /*
+         * If the builder has an entry handler
+         * then call it through the container and
+         * let it load the entry itself.
+         */
+        if (
+            (is_string($this->entry) && class_exists($this->entry))
+            || $this->entry instanceof \Closure
+        ) {
+
+            $entry = Resolver::resolve($this->entry, compact('builder'));
+
+            $this->entry = Evaluator::evaluate($entry ?: $this->entry, compact('builder'));
+
+            return;
+        }
+
+        /**
+         * If the builder already has
+         * an entry then just use that.
+         */
+        if ($this->entry && is_object($this->entry)) {
+
+            $this->instance->entry = $this->entry;
+
+            return;
+        }
+
+        /*
+         * Fallback to using the repository 
+         * to get and/or paginate the results.
+         */
+        if ($this->repository() instanceof RepositoryInterface) {
+
+            $this->criteria = $this->repository()->newCriteria();
+
+            $this->instance->entry = $this->criteria->find($this->entry);
+
+            return;
+        }
+    }
+
+    public function authorizeForm(FormAuthorizer $authorizer)
+    {
+        $authorizer->authorize($this);
+    }
+
+    public function setValidation()
+    {
+        if (!$this->stream) {
+            return;
+        }
+
+        $rules = array_merge(
+            (array) $this->rules,
+            (array) $this->stream->rules
+        );
+
+        $validators = array_merge(
+            (array) $this->validators,
+            (array) $this->stream->validators
+        );
+
+        $this->instance->rules = $this->rules = $rules;
+        $this->instance->validators = $this->validators = $validators;
+    }
+
+    public function buildFields()
+    {
+        dd('Form > Build Fields');
     }
 
     //---------------------------------------------------------------------
--- src/Table/TableBuilder.php
+++ src/Table/TableBuilder.php
@@ -3,7 +3,6 @@
 namespace Streams\Ui\Table;
 
 use Illuminate\Support\Arr;
-use Illuminate\Support\Str;
 use Streams\Ui\Table\Table;
 use Streams\Ui\Button\Button;
 use Streams\Ui\Support\Value;
@@ -12,8 +11,8 @@ use Streams\Ui\Support\Builder;
 use Streams\Ui\Support\Normalizer;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Request;
+use Streams\Ui\Table\Component\Row\Row;
 use Streams\Ui\Table\Component\View\View;
-use Streams\Ui\Table\Workflows\QueryTable;
 use Streams\Ui\Table\Component\Action\Action;
 use Streams\Ui\Table\Component\Column\Column;
 use Streams\Ui\Table\Component\Filter\Filter;
@@ -22,14 +21,7 @@ use Streams\Ui\Table\Component\View\ViewRegistry;
 use Streams\Ui\Table\Component\Action\ActionRegistry;
 use Streams\Ui\Table\Component\Button\ButtonRegistry;
 use Streams\Ui\Table\Component\Filter\FilterRegistry;
-use Streams\Ui\Table\Component\Row\Workflows\BuildRows;
 use Streams\Core\Repository\Contract\RepositoryInterface;
-use Streams\Ui\Support\Component;
-use Streams\Ui\Table\Component\Action\Workflows\BuildActions;
-use Streams\Ui\Table\Component\Button\Workflows\BuildButtons;
-use Streams\Ui\Table\Component\Column\Workflows\BuildColumns;
-use Streams\Ui\Table\Component\Filter\Workflows\BuildFilters;
-use Streams\Ui\Table\Component\Row\Row;
 
 /**
  * Class TableBuilder
@@ -87,16 +79,6 @@ class TableBuilder extends Builder
                 'make_buttons' => [$this, 'makeButtons'],
                 'make_columns' => [$this, 'makeColumns'],
                 'make_rows' => [$this, 'makeRows'],
-            ],
-
-            'workflows' => [
-                'rows' => BuildRows::class,
-                //'build' => BuildTable::class,
-                //'query' => QueryTable::class,
-                //'actions' => BuildActions::class,
-                //'filters' => BuildFilters::class,
-                'columns' => BuildColumns::class,
-                //'buttons' => BuildButtons::class,
             ],
         ], $attributes));
     }
