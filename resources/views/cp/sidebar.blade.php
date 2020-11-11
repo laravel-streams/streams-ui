d0478ce9d76208f0e30848b3752cd4f20d845d76
Ryan Thompson <ryan@pyrocms.com>
2020-11-10 23:51:41 -0600

    Ah

--- resources/views/cp/sidebar.blade.php
+++ resources/views/cp/sidebar.blade.php
@@ -3,11 +3,11 @@
 
         <div class="flex flex-col flex-grow bg-black pb-4 overflow-y-auto">
             
-            <div class="mt-5 flex-1 flex flex-col">
+            <div class="mt-5 flex flex flex-col">
                 @include('ui::cp.navigation')
             </div>
 
-            <div class="px-4 flex align-self-bottom flex-col text-white">
+            <div class="px-4 mt-4 flex-1 flex-col text-white opacity-25 text-xs">
                 {{ number_format(microtime(true) - Request::server('REQUEST_TIME_FLOAT'), 2) . ' s' }}&nbsp;|&nbsp;
                 @php
                     $size = memory_get_usage(true);
