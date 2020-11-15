0d0aa5fde691c922fb743e35473e2049d6f77c4b
William Åström <william@pixney.com>
2020-11-15 17:33:57 +0100

    :collision: Fixed

--- resources/views/cp/cp.blade.php
+++ resources/views/cp/cp.blade.php
@@ -8,10 +8,10 @@
 <body>
     <!-- cp.blade.php -->
     <div class="grid grid-cols-16 min-h-screen">
-        <aside class="col-span-16 md:col-span-3 lg:col-span-3 xl:col-span-2">
+        <aside class="col-span-16 md:col-span-5 lg:col-span-4 xl:col-span-3 xxxl:col-span-2">
             @include('ui::cp.sidebar')
         </aside>
-        <main class="col-span-16 md:col-span-13 lg:col-span-12 xl:col-span-14 xxxl:col-span-14 bg-gray-100">
+        <main class="col-span-16 md:col-span-11 lg:col-span-12 xl:col-span-13 xxxl:col-span-14 bg-gray-100">
 
             @include('ui::cp.top')
             @include('ui::cp.content')
