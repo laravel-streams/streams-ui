1cb9094f70127165f2b56cb7ae62ba2eb710250b
Ryan Thompson <service@aiwebsystems.com>
2020-12-01 12:52:43 -0600

    Aha

--- src/ControlPanel/ControlPanel.php
+++ src/ControlPanel/ControlPanel.php
@@ -4,17 +4,9 @@ namespace Streams\Ui\ControlPanel;
 
 use Streams\Ui\Support\Component;
 use Streams\Ui\Button\ButtonCollection;
-use Streams\Ui\ControlPanel\Component\Section\SectionCollection;
 use Streams\Ui\ControlPanel\Component\Shortcut\ShortcutCollection;
 use Streams\Ui\ControlPanel\Component\Navigation\NavigationCollection;
 
-/**
- * Class ControlPanel
- *
- * @link   http://pyrocms.com/
- * @author PyroCMS, Inc. <support@pyrocms.com>
- * @author Ryan Thompson <ryan@pyrocms.com>
- */
 class ControlPanel extends Component
 {
 
