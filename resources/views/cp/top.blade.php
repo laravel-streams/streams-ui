974ffdc6fb71b5a4f38dd784c8036135ca0a7df6
Ryan Thompson <ryan@pyrocms.com>
2020-12-02 00:43:20 -0600

    Icons

--- resources/views/cp/top.blade.php
+++ resources/views/cp/top.blade.php
@@ -30,7 +30,7 @@
                     @if ($shortcut->svg)
                     {{-- {!! $shortcut->svg !!} --}}
                     @elseif ($shortcut->icon)
-                    <x-{{ $shortcut->icon }}/>
+                    {{ svg($shortcut->icon, ['class' => 'h-8 w-8']) }}
                     @elseif ($shortcut->image)
                     <img class="h-8 w-8 rounded-full" src="{{ $shortcut->image }}" alt="">
                     @elseif ($shortcut->text)
