bf5e91f5f644639fcd89c475b9d6ada4c07385ae
Ryan Thompson <ryan@pyrocms.com>
2020-12-02 00:09:40 -0600

    update

--- resources/views/input/toggle.blade.php
+++ resources/views/input/toggle.blade.php
@@ -2,7 +2,6 @@
 <label class="inline-flex items-center mt-3">
     <input {!! $input->htmlAttributes([
         'type' => 'checkbox',
-        'class' => 'rounded text-pink-500',
         'checked' => ($input->field->value),
     ]) !!} checked><span class="ml-2 text-gray-700">Toggle</span>
 </label>
