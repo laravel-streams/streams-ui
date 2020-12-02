ece893f33b56e9c1d1b7e82f68aa9c6b81b4134a
Ryan Thompson <ryan@pyrocms.com>
2020-12-02 11:14:32 -0600

    Users

--- resources/views/cp/brand.blade.php
+++ resources/views/cp/brand.blade.php
@@ -1,5 +1,5 @@
 <div class="flex flex-col pt-5 pb-5 overflow-y-auto">
-    <div class="flex items-center flex-shrink-0 px-4 h-8">
+    <div class="flex items-center flex-shrink-0 px-4 h-6">
         <a class="logo-link font-mono text-2xl">
             {{ __(config('app.name')) }}
         </a>
