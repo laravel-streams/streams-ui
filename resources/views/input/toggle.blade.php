7b008dc8d74cad46868fb253c0ca0751251ab6e8
Ryan Thompson <ryan@pyrocms.com>
2020-12-02 23:02:39 -0600

    Toggle

--- resources/views/input/toggle.blade.php
+++ resources/views/input/toggle.blade.php
@@ -3,7 +3,7 @@
     <!-- This example requires Tailwind CSS v2.0+ -->
     <!-- On: "bg-indigo-600", Off: "bg-gray-200" -->
     <button type="button" class="relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" x-bind:class="{ 
-        'bg-indigo-600' : checked,
+        'bg-black' : checked,
         'bg-gray-200' : !checked,
       }" x-on:click="checked == true ? checked = false : checked = true;">
         <span class="sr-only">Use setting</span>
