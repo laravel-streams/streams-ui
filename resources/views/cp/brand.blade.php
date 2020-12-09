f6d1e86f6cfd5253e0064aff7a044e970067df07
Ryan Thompson <ryan@pyrocms.com>
2020-12-09 01:13:45 -0600

    Update name

--- resources/views/cp/brand.blade.php
+++ resources/views/cp/brand.blade.php
@@ -1,7 +1,11 @@
 <div class="flex h-16 overflow-y-auto border-b-2 border-primary">
     <div class="flex items-center px-4">
         <a class="logo-link font-mono text-2xl">
+            @if ($theme = Streams::entries('theme')->find('settings'))
+            {{ $theme->name ?: __(config('app.name')) }}
+            @else
             {{ __(config('app.name')) }}
+            @endif
         </a>
     </div>
 </div>
