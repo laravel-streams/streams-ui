815e9ae4c5bc54ad098bbace15df96f35c99ff2a
Ryan Thompson <ryan@pyrocms.com>
2020-12-22 13:53:57 -0600

    Fixed

--- resources/public/js/css/inputs/markdown.d.ts
+++
@@ -1,1 +0,0 @@
-import '../../../scss/inputs/markdown.scss';
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,23 +1,20 @@
 const mix = require('laravel-mix');
 const tailwindcss = require('tailwindcss');
-const path = require('path');
-
-const isDev = process.env.NODE_ENV === 'development';
 
 mix
-    .ts('resources/ts/index.ts', 'resources/js')
-    // .sass('resources/scss/theme.scss', 'resources/css')
-    // .sass('resources/scss/variables.scss', 'resources/css')
-    // .sass('resources/scss/inputs/markdown.scss', 'resources/css/inputs')
+    .ts('resources/ts/index.ts', 'resources/public/js')
+    // .sass('resources/scss/theme.scss', 'resources/public/css')
+    // .sass('resources/scss/variables.scss', 'resources/public/css')
+    // .sass('resources/scss/inputs/markdown.scss', 'resources/public/css/inputs')
+    .copyDirectory('resources/public', '../../../public/vendor/streams/ui')
     .options({
         processCssUrls: false,
         postCss: [tailwindcss('./tailwind.config.js')],
     })
-    .copyDirectory('resources/public', '../../../public/vendor/streams/ui')
     .webpackConfig(
         function (webpack) {
             return {
-                //devtool: isDev ? 'hidden-source-map' : false,
+
                 plugins: [
                     require('@tailwindcss/ui'),
                 ],
@@ -25,14 +22,16 @@ mix
                 externals: {
                     '@streams/core': ['streams', 'core'],
                 },
+
                 output: {
+
+                    library: ['streams', 'ui'],
+                    libraryTarget: 'window',
 
                     //path: path.resolve('./resources/public'),
                     //filename: 'js/[name].js',
                     //chunkFilename: 'js/chunk.[name].js',
-                    library: ['streams', 'ui'],
                     //publicPath: '/vendor/streams/ui/',
-                    libraryTarget: 'window',
                 }
             };
         }
