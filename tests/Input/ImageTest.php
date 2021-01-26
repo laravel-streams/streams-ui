d433acae972c3a3337edd150bc23c72abc3d6fff
Ryan Thompson <ryan@pyrocms.com>
2021-01-25 22:45:07 -0600

    Inputs

--- tests/Input/ImageTest.php
+++ tests/Input/ImageTest.php
@@ -3,10 +3,11 @@
 namespace Streams\Ui\Tests\Input;
 
 use Tests\TestCase;
-use Streams\Ui\Input\File;
+use Streams\Ui\Input\Image;
+use Illuminate\Support\Facades\File;
 use Streams\Core\Support\Facades\Streams;
 
-class FileTest extends TestCase
+class ImageTest extends TestCase
 {
 
     public function setUp(): void
@@ -15,28 +16,28 @@ class FileTest extends TestCase
 
         Streams::load(base_path('vendor/streams/ui/tests/litmus.json'));
     }
-
-    public function testInput()
-    {
-        $entry = Streams::repository('testing.litmus')->find('field_types');
-        $input = $entry->stream()->fields->file->input();
-
-        $this->assertInstanceOf(File::class, $input);
-    }
-
-    public function testAttributes()
-    {
-        $entry = Streams::repository('testing.litmus')->find('field_types');
-        $field = $entry->stream()->fields->file;
-        $input = $field->input();
-
-        $this->assertStringContainsString('type="file"', $input->htmlAttributes());
-    }
-
+    
     public function tearDown(): void
     {
         if (is_dir($dir = base_path('vendor/streams/ui/tests/data/uploads'))) {
             File::deleteDirectory($dir);
         }
+    }
+
+    public function testInput()
+    {
+        $entry = Streams::repository('testing.litmus')->find('field_types');
+        $input = $entry->stream()->fields->image->input();
+
+        $this->assertInstanceOf(Image::class, $input);
+    }
+
+    public function testAttributes()
+    {
+        $entry = Streams::repository('testing.litmus')->find('field_types');
+        $field = $entry->stream()->fields->image;
+        $input = $field->input();
+
+        $this->assertStringContainsString('type="file"', $input->htmlAttributes());
     }
 }
