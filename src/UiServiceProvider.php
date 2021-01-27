067cf421ef8ad02d2ac290aa2724036459fac9e7
Robin Radic <rradic@hotmail.com>
2021-01-27 11:59:23 +0100

    fix merge fail

--- src/UiServiceProvider.php
+++ src/UiServiceProvider.php
@@ -4,8 +4,10 @@ namespace Streams\Ui;
 
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Lang;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Str;
