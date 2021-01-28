93cbe6dc72f0d728bbf2e83606de976ff396e730
Robin Radic <rradic@hotmail.com>
2021-01-28 22:37:27 +0100

    markdown component

--- resources/views/input/markdown.blade.php
+++ resources/views/input/markdown.blade.php
@@ -4,17 +4,6 @@
 <textarea {!! $input->htmlAttributes([
     'rows' => 10,
     'x-data' => "window.streams.core.app.get('input.markdown')({$input->toJson()})",
-    'x-init' => 'init()'
-]) !!}>{{ $input->field->value }}</textarea>
-
-{{--receive this? gimme a SS--}}
-<script>
-{{--window.addEventListener('load', () => {--}}
-{{--    window.streams.core.app.get('input.markdown')--}}
-{{--        .init("#{{ $input->field->handle }}-input")--}}
-{{--        .then(function (input) {--}}
-{{--            console.log("#{{ $input->field->handle }}-input", input);--}}
-{{--            return input;--}}
-{{--        });--}}
-{{--});--}}
-</script>
+    'x-init' => 'init()',
+    'x-model' => 'value'
+]) !!}></textarea>
