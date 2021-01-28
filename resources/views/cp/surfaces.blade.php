b194b2b46e7bcceaa5f3501b14d1547b713cc561
Ryan Thompson <ryan@pyrocms.com>
2021-01-28 17:31:45 -0600

    Hide these for now.

--- resources/views/cp/surfaces.blade.php
+++ resources/views/cp/surfaces.blade.php
@@ -1,4 +1,4 @@
-<div x-data="streams.core.app.get('surfaces')()">
+{{-- <div x-data="streams.core.app.get('surfaces')()">
     <div class="absolute top-0 left-0 h-screen w-screen bg-white dark:bg-black z-40 border-8 border-black dark:border-white overflow-scroll" x-show="isOpen()">
         <div x-html="content">...</div>
         <div class="text-2xl text-black dark:text-white cursor-pointer fixed top-0 right-0" @click="isOpen() ? close() : show();"
@@ -6,4 +6,4 @@
         x-on:close-surfaces.window="close()"
         >Close</div>
     </div>
-</div>
+</div> --}}
