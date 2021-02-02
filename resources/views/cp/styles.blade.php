74ed84fed9470a8d48775d0c6c1f3f18bffb8ec3
Ryan Thompson <service@aiwebsystems.com>
2021-02-02 13:13:39 -0600

    Get it

--- resources/views/cp/styles.blade.php
+++ resources/views/cp/styles.blade.php
@@ -1,5 +1,5 @@
 <!-- CSS -->
-<link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;700&display=swap" rel="stylesheet">
+{{-- <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;700&display=swap" rel="stylesheet"> --}}
 
 {{-- <link href="/vendor/streams/ui/js/index.css" rel="stylesheet"> --}}
 <link href="/vendor/streams/ui/css/theme.css" rel="stylesheet">
