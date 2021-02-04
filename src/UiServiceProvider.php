7728c8ebbe7df9c840f0f990fde4435470330b5b
Ryan Thompson <service@aiwebsystems.com>
2021-02-04 14:25:11 -0600

    memory

--- src/UiServiceProvider.php
+++ src/UiServiceProvider.php
@@ -257,7 +257,7 @@ class UiServiceProvider extends ServiceProvider
 
         Field::macro('input', function (array $attributes = []) {
 
-            return $this->once($this->stream . '.' . $this->handle . '.' . $this->type, function () use ($attributes) {
+            return $this->once($this->stream->handle . '.' . $this->handle . '.' . $this->type, function () use ($attributes) {
 
                 $attributes['field'] = Arr::get($attributes, 'field', $this);
 
