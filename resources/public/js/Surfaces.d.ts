ff49b39a07dcd17afba337b9e72fb8f5accea4b0
Ryan Thompson <ryan@pyrocms.com>
2021-02-06 12:03:00 -0600

    UI prep for RPAC

--- resources/public/js/Surfaces.d.ts
+++
@@ -1,9 +0,0 @@
-declare const _default: {
-    show: boolean;
-    content: any;
-    open(): void;
-    close(): void;
-    isOpen(): boolean;
-    load(url: any): void;
-};
-export default _default;
