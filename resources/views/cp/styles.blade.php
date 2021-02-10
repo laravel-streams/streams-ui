1db6ca5b2a6a22eba130a990ebb6afe922e1fc51
Ryan Thompson <service@aiwebsystems.com>
2021-02-10 16:23:14 -0600

    Ugh

--- resources/views/cp/styles.blade.php
+++ resources/views/cp/styles.blade.php
@@ -1,6 +1,6 @@
 <!-- CSS -->
-{{ Assets::load('styles', 'ui::css/tailwind.css') }}
 {{ Assets::load('styles', 'ui::css/theme.css') }}
+{{ Assets::load('styles', 'ui::css/tailwind.css') }}
 
 <style>
     :root {
@@ -10,6 +10,13 @@
         --cp-color-light: {{ $theme->light ?: "#ffffff" }};
         --cp-color-dark: {{ $theme->dark ?: "#000000" }};
         --cp-color-primary: {{ $theme->primary ?: "#4e42d9" }};
+
+        --topbar-bg-color: {{ $theme->topbar_bg_color ?: "#cccccc" }};
+        --topbar-text-color: {{ $theme->topbar_text_color ?: "#000000" }};
+        --sidebar-bg-color: {{ $theme->sidebar_bg_color ?: "#cccccc" }};
+        --sidebar-text-color: {{ $theme->sidebar_text_color ?: "#000000" }};
+        --input-bg-color: {{ $theme->input_bg_color ?: "#ffffff" }};
+        --input-text-color: {{ $theme->input_text_color ?: "#000000" }};
     }
 </style>
 
