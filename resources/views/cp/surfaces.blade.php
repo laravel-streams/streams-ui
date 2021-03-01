a8a4f57c45ae4db6b131c069db3ae7a85ccce9b3
Ryan Thompson <service@aiwebsystems.com>
2021-03-01 17:26:08 -0600

    Observe

--- resources/views/cp/surfaces.blade.php
+++ resources/views/cp/surfaces.blade.php
@@ -3,7 +3,7 @@
     <div class="absolute top-0 left-0 h-screen w-screen bg-white dark:bg-black z-40 border-8 border-black dark:border-white overflow-scroll" x-show="enabled" x-cloak>
         {{-- <div x-html="content">...</div> --}}
         <div class="text-2xl text-black dark:text-white cursor-pointer fixed top-0 right-0" @click="enabled === true ? disableSurfaces() : enableSurfaces();"
-        x-on:enable-surfaces.window="alert(); enableSurfaces(); load($event.detail.url)"
+        x-on:enable-surfaces.window="enableSurfaces();"
         x-on:disable-surfaces.window="disableSurfaces()"
         >Close</div>
     </div>
@@ -29,7 +29,7 @@
 
                 disableSurfaces() {
                     
-                    this.enabled = true;
+                    this.enabled = false;
 
                     return this;
                 }
