a747b676192f588e0475eab2acc70bff1c2ef5fb
Ryan Thompson <ryan@pyrocms.com>
2021-03-09 23:37:16 -0600

    Placeholder

--- resources/views/input/relationship.blade.php
+++ resources/views/input/relationship.blade.php
@@ -1,6 +1,11 @@
 <select {!! $input->htmlAttributes([
     'type' => null,
 ]) !!}>
+
+@if (!$input->field->isRequired())
+    <option>---</option>
+@endif
+
 @foreach ($input->options() as $key => $value)
     <option {{ $key == $input->value ? 'selected' : null }} value="{{ $key }}">{{ $value }}</option>
 @endforeach
