15fef994dfc978a0675f793d0577679cf7fc45d5
Ryan Thompson <service@aiwebsystems.com>
2021-05-14 01:02:43 -0500

    Toggle

--- resources/views/input/markdown.blade.php
+++ resources/views/input/markdown.blade.php
@@ -3,7 +3,8 @@
 <!-- markdown.blade.php -->
 <textarea {!! $input->htmlAttributes([
     'rows' => 10,
-    'x-data' => "app.get('input.markdown')({$input->toJson()})",
-    'x-init' => 'init()',
+    //'x-data' => "sterams.core.app.get('input.markdown')({$input->toJson()})",
+    'x-data' => $input->toJson(),
+    //'x-init' => 'init()',
     'x-model' => 'value'
 ]) !!}></textarea>
