5e540be4a20ff1a25e2a27c0dfcf9f6eb295cac0
Ryan Thompson <ryan@pyrocms.com>
2022-03-17 20:33:16 -0500

    Not working

--- src/Button/ButtonBladeComponent.php
+++ src/Button/ButtonBladeComponent.php
@@ -5,11 +5,9 @@ namespace Streams\Ui\Component\Components;
 use Illuminate\View\Component;
 use Illuminate\Support\Facades\View;
 
-class ButtonComponent extends Component
+class ButtonBladeComponent extends Component
 {
-    public $componentName = 'button';
-
-    public function __construct(public string $type)
+    public function __construct()
     {
     }
 
