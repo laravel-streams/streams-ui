736c02b5cf7311f7236251d5e8cacf2ef82bf5ad
Ryan Thompson <ryan@pyrocms.com>
2022-03-29 16:03:27 -0500

    Rendering

--- src/Support/Builder.php
+++ src/Support/Builder.php
@@ -2,15 +2,33 @@
 
 namespace Streams\Ui\Support;
 
+use Streams\Core\Stream\Stream;
 use Streams\Ui\Support\Component;
 use Illuminate\Support\Collection;
 use Streams\Core\Support\Workflow;
+use Streams\Core\Support\Facades\Streams;
 
 class Builder extends Workflow
 {
     public array $steps = [
+        'cast_stream' => self::class . '@castStream',
         'load_attributes' => self::class . '@loadAttributes',
     ];
+
+    public function castStream(Collection $attributes)
+    {
+        if (!$stream = $attributes->get('stream')) {
+            return;
+        }
+
+        if ($stream instanceof Stream) {
+            return;
+        }
+
+        if (is_string($stream)) {
+            $attributes->put('stream', Streams::make($stream));
+        }
+    }
 
     public function loadAttributes(Component $component, Collection $attributes)
     {
