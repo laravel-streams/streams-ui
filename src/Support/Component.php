d6b8bbbb7b4d7fac359c1ebe462b0241e3b8a229
Ryan Thompson <ryan@pyrocms.com>
2023-02-09 12:24:34 -0600

    Livewire testing

--- src/Support/Component.php
+++ src/Support/Component.php
@@ -26,18 +26,6 @@ abstract class Component extends \Livewire\Component
 
     public string $template;
 
-    // public function __construct(array $attributes = [])
-    // {
-    //     $this->__constructPrototype($attributes);
-
-    //     $this->id = $this->id ?? Str::random(20);
-
-    //     Cache::put($this->id, json_encode([
-    //         'component' => static::class,
-    //         'attributes' => $attributes,
-    //     ]));
-    // }
-
     public function stream(): Stream
     {
         return $this->once(__METHOD__ . '.' . $this->stream, fn ()  => Streams::make($this->stream));
@@ -48,35 +36,14 @@ abstract class Component extends \Livewire\Component
         $payload['component'] = $this;
 
         if (strpos($this->template, '<') !== false) {
-            $rendered = View::parse($this->template, $payload)->render();
-        } else {
-            $rendered = View::make($this->template, $payload)->render();
+            return View::parse($this->template, $payload)->render();
         }
-
-        return $rendered;
-        //return $this->finishRender($rendered);
+        
+        return View::make($this->template, $payload)->render();
     }
 
     public function name()
     {
         return $this->once(__METHOD__ . static::class, fn () => Str::kebab(class_basename($this)));
     }
-
-    // public function toArray()
-    // {
-    //     return Hydrator::dehydrate($this);
-    // }
-
-    // protected function finishRender(string $rendered): string
-    // {
-    //     $attributes = HtmlFacade::attributes([
-    //         'ui:id' => $this->id,
-    //         'ui:name' => $this->name(),
-    //         'ui:data' => json_encode(Hydrator::dehydrate($this)),
-    //     ]);
-
-    //     $rendered = preg_replace('/(<div\b[^><]*)>/i', '$1 ' . $attributes . '>', $rendered);
-
-    //     return $rendered;
-    // }
 }
