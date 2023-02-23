004fe860534deabd4e0b0a315dec1f32168098c8
Ryan Thompson <ryan@pyrocms.com>
2023-02-23 16:54:10 -0600

    We can redirect later.

--- src/Components/Admin.php
+++ src/Components/Admin.php
@@ -20,8 +20,7 @@ class Admin extends Component
         }
 
         if ($entry = Request::segment(3)) {
-            // IF there is a "view" component
-            //Breadcrumbs::put("admin/$stream/$entry", ucwords(Str::humanize($entry)));
+            Breadcrumbs::put("admin/$stream/$entry", ucwords(Str::humanize($entry)));
         }
 
         if ($action = Request::segment(4)) {
