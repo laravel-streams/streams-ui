e894280b7ad1df865979147ad4ae837c83bdf470
Ryan Thompson <ryan@pyrocms.com>
2024-05-16 06:33:23 -0500

    Stats

--- resources/views/builders/stats.blade.php
+++ resources/views/builders/stats.blade.php
@@ -2,18 +2,19 @@
     $columns = 3;//$this->getColumns();
 @endphp
 
-<x-ui::widget>
+<x-ui::widget class="w-full">
     <div
         @if ($pollingInterval = $this->getPollingInterval())
             wire:poll.{{ $pollingInterval }}
         @endif
         @class([
-            'grid gap-6 mb-6',
-            'md:grid-cols-1' => $columns === 1,
-            'md:grid-cols-2' => $columns === 2,
-            'md:grid-cols-3' => $columns === 3,
-            'md:grid-cols-2 xl:grid-cols-4' => $columns === 4,
-        ])>
+            'w-full',
+            // 'md:grid-cols-1' => $columns === 1,
+            // 'md:grid-cols-2' => $columns === 2,
+            // 'md:grid-cols-3' => $columns === 3,
+            // 'md:grid-cols-2 xl:grid-cols-4' => $columns === 4,
+        ])
+        >
         @foreach ($this->getStats() as $stat)
             {{ $stat->render() }}
         @endforeach
