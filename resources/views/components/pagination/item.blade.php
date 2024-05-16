99ada1ff805ded064a3c3087808b3c97ae30229b
Ryan Thompson <ryan@pyrocms.com>
2024-05-16 15:02:04 -0500

    Birthday Cake

--- resources/views/components/pagination/item.blade.php
+++ resources/views/components/pagination/item.blade.php
@@ -11,7 +11,7 @@
 <li
     {{
         $attributes->class([
-            'group/item border-x-[0.5px] border-gray-200 first:border-s-0 last:border-e-0 dark:border-white/10',
+            'group/item border-x-[0.5px] border-gray-200 first:border-s-0 last:border-e-0',
             '' => $disabled,
             '' => $active,
         ])
@@ -22,16 +22,16 @@
         @disabled($disabled)
         type="button"
         @class([
-            'group/button relative flex overflow-hidden p-2 outline-none transition duration-75 group-first/item:rounded-s-lg group-last/item:rounded-e-lg',
-            'hover:bg-gray-50 focus-visible:z-10 focus-visible:ring-2 focus-visible:ring-primary-600 dark:hover:bg-white/5 dark:focus-visible:ring-primary-500' => ! $disabled,
-            'bg-gray-50 dark:bg-white/5' => $active,
+            'group/button relative flex items-center overflow-hidden p-2 outline-none transition duration-75 group-first/item:rounded-s-lg group-last/item:rounded-e-lg',
+            'hover:bg-gray-50 focus-visible:z-10 focus-visible:ring-2 focus-visible:ring-primary-600' => ! $disabled,
+            'bg-gray-50' => $active,
         ])
     >
         @if (filled($icon))
             <x-ui::icon
                 :alias="$iconAlias"
                 :icon="$icon"
-                class="h-5 w-5 text-gray-400 transition duration-75 group-hover/button:text-gray-500 dark:text-gray-500 dark:group-hover/button:text-gray-400"
+                class="h-5 w-5 text-gray-400 transition duration-75 group-hover/button:text-gray-500"
             />
         @endif
 
@@ -39,9 +39,9 @@
             <span
                 @class([
                     'px-1.5 font-semibold',
-                    'text-gray-700 dark:text-gray-200' => ! ($disabled || $active),
-                    'text-gray-500 dark:text-gray-400' => $disabled,
-                    'text-primary-600 dark:text-primary-400' => $active,
+                    'text-gray-700' => ! ($disabled || $active),
+                    'text-gray-500' => $disabled,
+                    'text-primary-600' => $active,
                 ])
             >
                 {{ $label ?? '...' }}
