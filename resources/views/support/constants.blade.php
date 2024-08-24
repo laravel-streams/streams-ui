d42ef2fb5368ba910a1083975de8dd5eeb073444
Ryan Thompson <ryan@pyrocms.com>
2024-08-24 18:41:57 -0500

    ENV goes away with optimization.

--- resources/views/support/constants.blade.php
+++ resources/views/support/constants.blade.php
@@ -3,7 +3,7 @@
 <script type="text/javascript">
     const APP_DEBUG = "{{ config('app.debug') }}";
     const APP_URL = "{{ config('app.url') }}";
-    const APP_ENV = "{{ env('APP_ENV') }}";
+    const APP_ENV = "{{ app()->environment() }}";
     const REQUEST_ROOT = "{{ request()->root() }}";
     const REQUEST_ROOT_PATH = "{{ \Illuminate\Support\Arr::get(parse_url(request()->root()), 'path') }}";
     const CSRF_TOKEN = "{{ csrf_token() }}";
