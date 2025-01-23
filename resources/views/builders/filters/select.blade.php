40fc2fb5890003e2018bc2a18e682d22df7e1e51
Ryan Thompson <ryan@pyrocms.com>
2025-01-22 23:08:07 -0600

    not sure if this is necessary:

--- resources/views/builders/filters/select.blade.php
+++ resources/views/builders/filters/select.blade.php
@@ -2,8 +2,7 @@
     id="{{ $filter->getName() }}-filter"
     name="{{ $path = $filter->getName() . '-filter' }}"
     {{-- onchange="this.form.submit()" --}}
-    wire:model.live="tableFilters.{{ $filter->getName() }}.value"
-    required>
+    wire:model.live="tableFilters.{{ $filter->getName() }}.value">
     
     <option {{ $filter->isRequired() ? 'disabled' : null }} value="">{{ $filter->getPlaceholder() ?: Str::title($filter->getName()) }}</option>
 
