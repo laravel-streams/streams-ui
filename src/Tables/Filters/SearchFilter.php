7138af28fa8bd1a997674b9377a22175404b3bd1
Ryan Thompson <ryan@pyrocms.com>
2025-07-25 10:02:00 -0500

    Search

--- src/Tables/Filters/SearchFilter.php
+++ src/Tables/Filters/SearchFilter.php
@@ -17,7 +17,7 @@ class SearchFilter extends Filter
         $this->query(function (Criteria $query, Table $table, $state): Criteria {
 
             $search = $state . '%';
-
+echo'Test';
             foreach ($table->getColumns() as $column) {
 
                 if (!$column->isSearchable()) {
