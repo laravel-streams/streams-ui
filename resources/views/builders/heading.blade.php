014096c6afb29ddf4d26be7f3f8f32c9ec99b88e
Ryan Thompson <ryan@pyrocms.com>
2025-12-02 12:49:19 -0600

    Update

--- resources/views/builders/heading.blade.php
+++ resources/views/builders/heading.blade.php
@@ -4,6 +4,10 @@
 
 @php
 
+    $icon = $heading->getIcon();
+    $iconPosition = 'before'; //$heading->getIconPosition() ?? 'before';
+    $iconSize = 'lg'; //$heading->getIconSize() ?? 'md';
+
     $priority = $heading->getPriority() ?? $priority;
     
     $classes = Arr::toCssClasses([
@@ -18,14 +22,40 @@
         //     default => null,
         // },
     ]);
+
+    $iconClasses = Arr::toCssClasses([
+        '',
+        match ($iconSize) {
+            'sm' => 'h-4 w-4',
+            'md' => 'h-5 w-5',
+            'lg' => 'h-6 w-6',
+            default => match ($priority) {
+            'h1' => 'h-6 w-6',
+            'h2' => 'h-5 w-5',
+            'h3' => 'h-4 w-4',
+            default => $iconSize,
+        },
+        },
+    ]);
 @endphp
 
 <div class="col-span-full flex items-center justify-between">
-    <div>
-        <{{ $priority }} class="{{ $classes }} font-bold text-gray-900">{{ $heading->getTitle() }}</{!! $priority !!}>
-        @if ($description = $heading->getDescription())
-        <p class="text-gray-500 mt-2">{!! $description !!}</p>
+    <div class="flex items-center space-x-3">
+        @if ($icon && $iconPosition === 'before')
+        <x-ui::icon
+            :attributes="
+                new \Illuminate\View\ComponentAttributeBag([
+                    'icon' => $icon,
+                    'class' => $iconClasses,
+                ])"
+        />
         @endif
+        <div>
+            <{{ $priority }} class="{{ $classes }} font-bold text-gray-900">{{ $heading->getTitle() }}</{!! $priority !!}>
+            @if ($description = $heading->getDescription())
+            <p class="text-gray-500 mt-2">{!! $description !!}</p>
+            @endif
+        </div>
     </div>
     @if ($actions = $heading->getActions())
     <div class="flex gap-2">
