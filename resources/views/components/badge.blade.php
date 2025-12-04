dbe3d3a90938c18bdd37b75ea118a9211fbe1a77
Ryan Thompson <ryan@pyrocms.com>
2025-12-04 01:43:24 -0600

    Diff

--- resources/views/components/badge.blade.php
+++ resources/views/components/badge.blade.php
@@ -74,7 +74,7 @@
                     default => $size,
                 },
                 match ($color) {
-                    'gray' => 'bg-gray-50 text-gray-600',
+                    'gray' => 'bg-gray-200 text-gray-600',
                     default => 'bg-custom-500 text-white',
                 },
             ])
