6e2825418d99ba3f6a21779bec5f7f1beba1888c
Ryan Thompson <ryan@pyrocms.com>
2026-02-04 13:45:39 -0600

    Rough-in

--- resources/views/builders/action-menu.blade.php
+++ resources/views/builders/action-menu.blade.php
@@ -26,15 +26,9 @@
          class="absolute right-0 mt-2 min-w-48 max-w-xs rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 z-50 overflow-hidden">
         <div class="py-1" role="menu">
             @foreach ($action->getActions() as $menuAction)
-                <a href="{{ $menuAction->getUrl() }}"
-                   @if($menuAction->shouldOpenInNewTab()) target="_blank" @endif
-                   class="flex items-center px-4 py-2 hover:bg-gray-100 text-nowrap {{ $menuAction->isDisabled() ? 'opacity-50 pointer-events-none' : '' }}"
-                   role="menuitem">
-                    @if($menuAction->getIcon())
-                        <x-ui::icon :name="$menuAction->getIcon()" class="mr-3 h-5 w-5" />
-                    @endif
-                    {{ $menuAction->getLabel() }}
-                </a>
+            <div>
+                {{ $menuAction->render() }}
+            </div>
             @endforeach
         </div>
     </div>
