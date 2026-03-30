ad0b1be913bbb4fe4f3bf434fdfc6405efc3c489
Ryan Thompson <ryan@pyrocms.com>
2026-03-30 13:31:55 -0500

    Update class for markdown input component

--- resources/views/components/inputs/markdown.blade.php
+++ resources/views/components/inputs/markdown.blade.php
@@ -18,7 +18,7 @@
                 $attributes
                     ->merge($getHtmlAttributes(), escape: false)
                     ->class([
-                        'max-w-full overflow-hidden rounded-lg bg-white font-mono text-base text-gray-950 shadow-sm ring-1 transition duration-75 focus-within:ring-2 dark:bg-white/5 dark:text-white sm:text-sm',
+                        'prose max-w-full overflow-hidden rounded-lg bg-white font-mono text-base text-gray-950 shadow-sm ring-1 transition duration-75 focus-within:ring-2 dark:bg-white/5 dark:text-white sm:text-sm',
                         'ring-gray-950/10 focus-within:ring-primary-600 dark:ring-white/20 dark:focus-within:ring-primary-500' => ! $errors->has($statePath),
                         'ring-danger-600 focus-within:ring-danger-600 dark:ring-danger-500 dark:focus-within:ring-danger-500' => $errors->has($statePath),
                     ])
