32e7c44972995594b3811ef8fc2bb36232b6efc5
Ryan Thompson <ryan@pyrocms.com>
2026-08-10 16:33:29 -0500

    Improve modals

--- resources/views/components/modals.blade.php
+++ resources/views/components/modals.blade.php
@@ -8,6 +8,13 @@
     if (! $action && method_exists($this, 'getMountedTableBulkAction')) {
         $action = $this->getMountedTableBulkAction();
     }
+
+    // Page actions dispatch open-modal with getId(); older markup appended "-action"
+    // and relied on isModalOpen() (always false unless set). Open from mount state so
+    // the dialog appears even when the Livewire open event races teleport/Alpine init.
+    $modalId = $action?->getId();
+    $shouldOpen = filled($action) && $action->shouldOpenModal();
+
 @endphp
 <div>
     @if ($action)
@@ -23,12 +30,14 @@
         :heading="$action?->getModalHeading()"
         :icon="$action?->getModalIcon()"
         :icon-color="$action?->getModalIconColor()"
-        :id="$action?->getId() . '-action'"
+        :id="$modalId"
         {{-- :slide-over="$action?->isModalSlideOver()" --}}
         :sticky-footer="$action?->isModalFooterSticky()"
         :sticky-header="$action?->isModalHeaderSticky()"
-        :visible="filled($action) && $action->shouldOpenModal()"
-        :open="json_encode($action?->isModalOpen() ?: false)"
+        {{-- :visible="filled($action) && $action->shouldOpenModal()" --}}
+        {{-- :open="json_encode($action?->isModalOpen() ?: false)" --}}
+        :visible="$shouldOpen"
+        :open="$shouldOpen"
         :width="$action?->getModalWidth()"
         :wire:key="$action ? $this->getId() . '.actions.' . $action->getName() . '.modal' : null"
         x-on:modal-closed.stop="$wire.unmountAction(false);">
